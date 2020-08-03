6b13816e72d692ea448fbceacec9f8b01cb7cf35
Pablo Garcia <pgarcia@onfocusdev.com>
2020-08-02 22:54:21 -0300

    Add material modules

--- src/app/sensors/sensors.module.ts
+++ src/app/sensors/sensors.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { SensorsRoutingModule } from './sensors-routing.module';
 import { SensorListComponent } from './sensor-list/sensor-list.component';
-
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { DialogComponent } from './dialog/dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { FormComponent } from './form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [ SensorListComponent],
+  declarations: [ SensorListComponent, DialogComponent, FormComponent],
   imports: [
     CommonModule,
-    SensorsRoutingModule
+    SensorsRoutingModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class SensorsModule { }
