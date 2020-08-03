0aca6ddc6223d4b3c5bd5b5e3ae654cf22ccb52d
Pablo Garcia <pgarcia@onfocusdev.com>
2020-08-02 22:55:22 -0300

    Add sensor services

---
+++ src/app/services/sensor.service.ts
@@ -0,0 +1,41 @@
+import { Sensor } from './../models/Sensor';
+import { catchError } from 'rxjs/operators';
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { handleHttpError } from '../utils/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SensorService {
+  uri = 'sensors';
+  sensorEndPoint = `${environment.apiUrl}/${this.uri}`;
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Sensor[]> {
+    return this.http
+      .get<Sensor[]>(this.sensorEndPoint)
+      .pipe(catchError(handleHttpError));
+  }
+
+  createSensor(sensor: Sensor): Observable<Sensor> {
+    return this.http
+      .post<Sensor>(this.sensorEndPoint, sensor)
+      .pipe(catchError(handleHttpError));
+  }
+
+  updateSensor(sensor: Sensor): Observable<Sensor> {
+    return this.http
+      .put<Sensor>(this.sensorEndPoint, sensor)
+      .pipe(catchError(handleHttpError));
+  }
+
+  removeSensor(sensorId: string): Observable<Sensor>{
+    return this.http
+      .delete<Sensor>(this.sensorEndPoint + sensorId)
+      .pipe(catchError(handleHttpError));
+  }
+}
